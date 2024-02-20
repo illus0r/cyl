cfe703094819a3771053ff0a6f5403296abc1e73
illus0r <zor667@gmail.com>
2024-02-21 01:16:58 +0300

    makeing the hat

--- index.js
+++ index.js
@@ -13,24 +13,80 @@ let mouse = [.5, .5];
 let gl = new Gl('canvas')
 let pr = new Pr(gl,
 	`#version 300 es
-			precision highp float;
-			uniform sampler2D tx;
-			uniform vec2 res;
-			uniform float time;
-			out vec4 o;
-			void main(){
-				vec2 uv = gl_FragCoord.xy/res;
-				o = uv.xyxy;
-				o.a=1.;
-			}`,
+precision highp float;
+uniform sampler2D tx;
+uniform sampler2D tx_rul;
+uniform float frame;
+uniform float pass;
+uniform float time;
+uniform vec2 res;
+uniform vec2 mouse;
+uniform float rndjs[4];
+out vec4 o;
 
-	`#version 300 es
-			in vec4 a_p;
-			void main(){
-					gl_Position = vec4(-1,-1,0,1);
-					if(gl_VertexID==1)gl_Position = vec4(3,-1,0,1);
-					if(gl_VertexID==2)gl_Position = vec4(-1,3,0,1);
-			}
+#define sabs(p) sqrt((p)*(p)+1e-3)
+#define smin(a,b) (a+b-sabs(a-b))*.5
+#define smax(a,b) (a+b+sabs(a-b))*.5
+
+#define rot(a) mat2(cos(a),-sin(a),sin(a),cos(a))
+
+int txId = 0;
+vec2 txUv = vec2(1.);
+
+float sdf(vec3 p){
+	p.z-=4.;
+	p.y+=.2;
+	p.zy*=rot(-.5+.1*sin(time));
+	vec3 pII = p;
+	p.xz = vec2(length(p.xz),0.);
+	vec3 pI = p;
+	float e=999.;
+	// ← 1
+
+	// sides
+	p = pI;
+	float sides=p.x-.3-p.y*.1;
+
+	// sides bottom
+	p=pI;
+	sides=max(-p.y,e);
+
+	// cap
+	p=pI;
+  float cap =p.y-.7;
+
+	p=pI;
+	p.x-=.5;
+	p.x=max(0.,p.x);
+	p.xy -= clamp(p.xy,-.01,.01);
+	e=min(length(p)-.0001,e);
+
+	// slice hat
+	e = max(-pII.z,e);
+
+
+	return e;
+}
+
+void main(){
+	vec2 uv = (gl_FragCoord.xy*2.-res)/res.y;
+	vec2 uvI = uv;
+
+	float j,d,e=1.;
+	vec3 p,pI;
+	for(;j++<99.&&e>1e-4;){
+		p=normalize(vec3(uv,6.))*d+.0001;
+
+		e=sdf(p);
+
+		d+=e;
+	}
+	o+=3./j;
+
+	o.rg*=txUv;
+	o.a=1.;
+}
+
 `)
 let prDr = new Pr(gl)
 
--- shader.frag
+++ shader.frag
@@ -1,6 +1,7 @@
 #version 300 es
 precision highp float;
 uniform sampler2D tx;
+uniform sampler2D tx_rul;
 uniform float frame;
 uniform float pass;
 uniform float time;
@@ -9,23 +10,50 @@ uniform vec2 mouse;
 uniform float rndjs[4];
 out vec4 o;
 
+#define sabs(p) sqrt((p)*(p)+1e-3)
+#define smin(a,b) (a+b-sabs(a-b))*.5
+#define smax(a,b) (a+b+sabs(a-b))*.5
+
+#define rot(a) mat2(cos(a),-sin(a),sin(a),cos(a))
+
 void main(){
 	vec2 uv = (gl_FragCoord.xy*2.-res)/res.y;
 	vec2 uvI = uv;
 	
-	mat2 z = mat2(uv,-uv.y,uv.x);
+	float j,d,e=1.;
+	vec3 p,pI;
+	for(;j++<99.&&e>1e-3;){
+		p=normalize(vec3(uv,1))*d+.0001;
+		p.z-=2.;
+		p.zy*=rot(-.5+.1*sin(time));
+		p.xz = vec2(length(p.xz),0.);
+		pI = p;
+		e=999.;
 
-	mat2 a = mat2(mouse,0,0);
-	a[1]=a[0].yx*vec2(-1,1);
 
-	for(int i=0;i<8;i++){
-		z *= z*z;
-		z = z + a;
+
+		p = pI;
+		e=smin(p.x-.3-p.y*.1,e);
+
+		p=pI;
+		e=max(-p.y,e);
+
+		p=pI;
+		e=smax(p.y-.7,e);
+
+		p=pI;
+		p.x-=.5;
+		p.x=smax(0.,p.x);
+		p.xy -= clamp(p.xy,-.01,.01);
+		e=min(length(p)-.01,e);
+
+
+		d+=e;
 	}
-
-	uv = z[0];
+	o+=3./j;
 
 	/* o.rg=fract(uv+time); */
-	o+=fract(length(uv.y)+time+atan(uvI.y,uvI.x)/3.1415/2.)/(length(uv));
+	/* /1* o.rg+=fract(uv); *1/ */
 	o.a=1.;
 }
+
